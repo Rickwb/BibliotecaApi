7c36b9f0c9fae9c906fb4e5b8bd72dd3e4237701
Rickwb <66922065+Rickwb@users.noreply.github.com>
2021-12-21 08:30:42 -0300

    adressDTO

---
+++ DTOs/CreateAdressDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotecaApi.DTOs
+{
+    public class CreateAdressDTO : BaseDTO
+    {
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string Uf { get; set; }
+        public string Ibge { get; set; }
+        public string Gia { get; set; }
+        public string Ddd { get; set; }
+        public string Siafi { get; set; }
+
+        public override void Valid()
+        {
+            if (Uf.Length != 2 || String.IsNullOrWhiteSpace(Uf)) IsValid = false;
+            if (Logradouro.Length > 100 || String.IsNullOrWhiteSpace(Logradouro)) IsValid = false;
+            if (String.IsNullOrWhiteSpace(Bairro)) IsValid = false;
+
+        }
+    }
+}
--- Entities/Adress.cs
+++ Entities/Adress.cs
@@ -2,6 +2,21 @@
 {
     public class Adress
     {
+        public Adress(string cep, string logradouro, string complemento, string bairro, string localidade,
+            string uf, string ibge, string gia, string ddd, string siafi)
+        {
+            Cep = cep;
+            Logradouro = logradouro;
+            Complemento = complemento;
+            Bairro = bairro;
+            Localidade = localidade;
+            Uf = uf;
+            Ibge = ibge;
+            Gia = gia;
+            Ddd = ddd;
+            Siafi = siafi;
+        }
+
         public string Cep { get; set; }
         public string Logradouro { get; set; }
         public string Complemento { get; set; }
